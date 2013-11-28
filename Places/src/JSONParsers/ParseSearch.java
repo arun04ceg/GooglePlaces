89037b5a67242edbfde3c3d1e1381b1eb609229f
arun04ceg <arun@Aruns-MacBook-Pro.local>
2013-11-28 01:33:31 -0700

    Wed

---
+++ Places/src/JSONParsers/ParseSearch.java
@@ -0,0 +1,18 @@
+package JSONParsers;
+
+import java.io.IOException;
+
+import DataBinders.SearchResultSet;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ParseSearch {
+
+	public static SearchResultSet parse(String data) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		SearchResultSet result = mapper.readValue(data, SearchResultSet.class);
+		return result;
+	}
+}
